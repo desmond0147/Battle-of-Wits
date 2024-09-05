b9292249cf92c9fbe5cabadfe5bca700c12cb61f
Omonuwa Desmond Osaigbovo <omonuwaosai@gmail.com>
2024-09-05 13:58:36 +0000

    Add initial board class with basic functionality

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+class Board:
+    """
+    Main board class. Sets the size, the number of ships, the player's name,
+    and the board type (player board or computer). Has methods for adding ships,
+    handling guesses, and printing the board.
+    """
+    def __init__(self, size, num_ships, player_name, is_computer=False):
+        self.size = size
+        self.num_ships = num_ships
+        self.player_name = player_name
+        self.is_computer = is_computer
+        self.board = [['~' for _ in range(size)] for _ in range(size)]
+        self.ships = set()
